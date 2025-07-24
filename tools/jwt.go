f943b045b910efd797caa40086e1967e5ebc7e51
zidnirifan <zidni.rifan7@gmail.com>
2025-07-24 16:17:27 +0700

    [feat][permission] add middleware

--- tools/jwt.go
+++ tools/jwt.go
@@ -7,6 +7,7 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 	"github.com/zidnirifan/varnion-utils/authentication"
+	"github.com/zidnirifan/varnion-utils/permission"
 )
 
 type Claims struct {
@@ -14,6 +15,21 @@ type Claims struct {
 	Role      string    `json:"role"`
 	LoginTime int64     `json:"login_time"`
 	jwt.RegisteredClaims
+}
+
+type PermissionClaims struct {
+	Data []*PermissionToken `json:"data"`
+	jwt.RegisteredClaims
+}
+
+type PermissionToken struct {
+	AppName string  `json:"app_name"`
+	Menus   []*Menu `json:"menus"`
+}
+
+type Menu struct {
+	MenuName   string    `json:"menu_name"`
+	Permission []*string `json:"permission"`
 }
 
 func ValidateAccessToken(tokenString string) (*Claims, error) {
@@ -25,6 +41,21 @@ func ValidateAccessToken(tokenString string) (*Claims, error) {
 		return nil, err
 	}
 	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+	return claims, nil
+}
+
+func ValidatePermissionToken(tokenString string) (*PermissionClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &PermissionClaims{}, func(token *jwt.Token) (interface{}, error) {
+		secret := os.Getenv(permission.PERMISSION_SECRET_KEY)
+		return []byte(secret), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	claims, ok := token.Claims.(*PermissionClaims)
 	if !ok || !token.Valid {
 		return nil, errors.New("invalid token")
 	}
