eb964cc00edd1ce2146ad63eaa50d37fea538ea1
zidnirifan <zidni.rifan7@gmail.com>
2025-07-29 09:17:16 +0700

    [feat][permission] add permission const

--- permission/permission.go
+++ permission/permission.go
@@ -3,3 +3,15 @@ package permission
 var (
 	PERMISSION_SECRET_KEY string = "PERMISSION_SECRET_KEY"
 )
+
+const (
+	ViewAll    = "view_all"
+	ViewDetail = "view_detail"
+	ViewSelf   = "view_self"
+	Create     = "create"
+	CreateBulk = "create_bulk"
+	Update     = "update"
+	UpdateSelf = "update_self"
+	Delete     = "delete"
+	DeleteBulk = "delete_bulk"
+)
