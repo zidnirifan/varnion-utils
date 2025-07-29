f97401b6f67a203e145bcf0a79231cfa1bc96dff
zidnirifan <zidni.rifan7@gmail.com>
2025-07-29 13:54:56 +0700

    [feat][error_handler]

---
+++ error_handler/error_handler.go
@@ -0,0 +1,31 @@
+package error_handler
+
+import (
+	"net/http"
+	"runtime"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"github.com/zidnirifan/varnion-utils/tools"
+)
+
+func HandleError(c *gin.Context, log *logrus.Logger, code int, err error) {
+	if code == http.StatusInternalServerError {
+		funcName := ""
+		if pc, _, _, ok := runtime.Caller(8); ok {
+			funcName = runtime.FuncForPC(pc).Name()
+		}
+		log.WithField("func", funcName).Error(err)
+
+		c.AbortWithStatusJSON(code, tools.Response{
+			Status:  "error",
+			Message: "Internal Server Error",
+		})
+		return
+	}
+
+	c.AbortWithStatusJSON(code, tools.Response{
+		Status:  "error",
+		Message: err.Error(),
+	})
+}
